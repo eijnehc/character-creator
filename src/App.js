8c7232a8a2a15fb7b43e328724983ff9d736e868
Chen Jie <e0036922@u.nus.edu>
2021-04-18 20:23:31 +0800

    Complete ex4 (wrong)

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div
+      style={{
+        backgroundColor: 'hsl(195deg, 20%, 86%)',
+        bottom: '0%',
+        height: '40%',
+        top: '60%',
+      }}
+    >
       <CharacterEditor />
       <Footer />
-    </>
+    </div>
   );
 }
 
